0f41060c6f01904dbdd5c9835c23b21c78085fcd
JosueGuzman <107532411+JosueElutiche24@users.noreply.github.com>
2023-02-25 14:18:03 -0600

    Se trabvajo en las secciones de games

--- src/pages/games/memory/index.js
+++ src/pages/games/memory/index.js
@@ -1,19 +1,24 @@
 import Layout from "@/components/layout/Layout"
 import Link from "next/link"
+import { useState } from "react"
 
 export default function memory() {
 
-function defaultFunction(){
+  const [level, setLevel] = useState(undefined)
+  const [points, setPoints] = useState(0)
+  const [modal, setModal] = useState(true)
 
-}
+  function defaultFunction(){
+
+  }
 
   return (
     <Layout page="memory">
       <div className="w-screen h-screen">
         <header className="w-full h-1/7 bg-c_GrayBlue flex justify-around items-center">
           <Link className="bg-red-700 p-3" href={"/"}>Abandonar</Link>
-          <button onClick={defaultFunction} className="">Nivel : ?</button>
-          <button onClick={defaultFunction} className="">Puntos : ?</button>
+          <button onClick={defaultFunction} className="">Nivel : {level}</button>
+          <button onClick={defaultFunction} className="">Puntos : {points}</button>
           <button onClick={defaultFunction} className="bg-c_LightBlue p-3">Help</button>
         </header>
         <section className="w-full h-6/7 p-3 flex">
@@ -24,6 +29,12 @@ function defaultFunction(){
             <p>memory</p>
           </div>
         </section>
+        
+        {modal == true &&
+        <div className="fixed bottom-1/4 left-1/4 w-1/2 h-1/2 bg-c_DarckBlue">
+          <button onClick={()=>setModal(false)}>Close</button>
+        </div>
+        }
       </div>
     </Layout>
   )
--- src/pages/games/mines/index.js
+++ src/pages/games/mines/index.js
@@ -1,14 +1,23 @@
 import Layout from "@/components/layout/Layout"
 import Link from "next/link"
+import { useState } from "react"
 
 export default function mines() {
+  
+  const [level, setLevel] = useState(undefined)
+  const [points, setPoints] = useState(0)
+  const [modal, setModal] = useState(true)
+
+  function defaultFunction(){
+
+  }
   return (
     <Layout page="mines">
       <div className="w-screen h-screen">
         <header className="w-full h-1/7 bg-c_GrayBlue flex justify-around items-center">
           <Link className="bg-red-700 p-3" href={"/"}>Abandonar</Link>
-          <button onClick={defaultFunction} className="">Nivel : ?</button>
-          <button onClick={defaultFunction} className="">Puntos : ?</button>
+          <button onClick={defaultFunction} className="">Nivel : {level}</button>
+          <button onClick={defaultFunction} className="">Puntos : {points}</button>
           <button onClick={defaultFunction} className="bg-c_LightBlue p-3">Help</button>
         </header>
         <section className="w-full h-6/7 p-3 flex">
@@ -19,6 +28,12 @@ export default function mines() {
             <p>mines</p>
           </div>
         </section>
+        
+        {modal == true &&
+        <div className="fixed bottom-1/4 left-1/4 w-1/2 h-1/2 bg-c_DarckBlue">
+          <button onClick={()=>setModal(false)}>Close</button>
+        </div>
+        }
       </div>
     </Layout>
   )
