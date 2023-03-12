2a60527876496303e0d654e2a8bc1be0df3ec457
JosueGuzman <107532411+JosueElutiche24@users.noreply.github.com>
2023-03-12 14:34:16 -0600

    Estilos de la seccion Login terminados

--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -1,22 +1,41 @@
 import Layout from "@/components/layout/Layout"
 import Link from "next/link"
+import { useState } from "react"
 
 export default function Login() {
-  return(
+
+  const [mode, setMode] = useState(1)
+
+  function handleLogIn(e){
+    e.preventDefault()
+  }
+  function handleSingUp(e){
+    e.preventDefault()
+  }
+
+  if(mode == 1)
+    return(
     <Layout>
       <section className="w-screen h-screen flexAllCenter txtMd">
         <div className="w-2/3 h-5/6 flexAllCenter flex-col">
           <div className="w-full h-1/7 flexAllCenter">
             <h1 className="txtXl2 txtSecondary">Bienvenido a super Kimis</h1>
           </div>
-          <div className="bg-c_GrayBlue w-5/6 h-5/7 rounded-md">
+          <div className="bg-c_GrayBlue w-5/6 h-5/7 rounded-md flexAllCenter flex-col">
+
+          <p className=" txtLg w-full h-1/7 flexAllCenter">Bienvenido de nuevo</p>
+            <form className="w-1/2 h-5/7 flexAllCenter flex-col" onSubmit={handleLogIn}>
+              <input type="text" name="name" className="rounded-lg text-center bg-c_LightGrayBlue py-1 txtPrincipal m-4 focus:outline-none" placeholder='apodo'/>
+              <input type="text" name="password" className="rounded-lg text-center bg-c_LightGrayBlue py-1 txtPrincipal m-4 focus:outline-none" placeholder='contraseña'/>
+              <button className="bg-c_LightGrayBlue rounded-md px-4 py-2 transition-all hover:margins">Enviar</button>
+            </form>
 
           </div>
           <div className="w-full h-2/7 flexAllCenter flex-col justify-around">
-            <button className="bg-c_LightGrayBlue w-1/6 h-1/3">Invitado</button>
+            <button className="bg-c_LightGrayBlue w-1/6 h-1/3  rounded-lg transition-all hover:font-bold hover:bg-c_DarckBlue hover:margins">Invitado</button>
             <div className="flexAllCenter flex-col">
               <p className="">No tienes una cuenta?</p>
-              <p className="text-c_Pink">Sing Up</p>
+              <button className="text-c_Pink hover:font-bold hover:text-white transition-all" onClick={()=>setMode(2)}>Crear cuenta</button>
             </div>
           </div>
         </div>
@@ -24,15 +43,35 @@ export default function Login() {
       </section>
     </Layout>
   )
-  return (
-    <Layout>
-      <secction>
-          <div>this is login</div>
-          <div>
-            <Link href={"/"}>go to Home</Link>
-            <Link href={"/support"}>go to ?</Link>
+  if(mode == 2)
+    return (
+      <Layout>
+      <section className="w-screen h-screen flexAllCenter txtMd">
+        <div className="w-2/3 h-5/6 flexAllCenter flex-col">
+          <div className="w-full h-1/7 flexAllCenter">
+            <h1 className="txtXl2 txtSecondary">Bienvenido a super Kimis</h1>
           </div>
-      </secction>
+          <div className="bg-c_GrayBlue w-5/6 h-5/7 rounded-md flexAllCenter flex-col">
+
+            <p className=" txtLg w-full h-1/7 flexAllCenter">Crear una cuenta</p>
+            <form className="w-1/2 h-5/7 flexAllCenter flex-col" onSubmit={handleSingUp}>
+              <input type="text" name="name" className="rounded-lg text-center bg-c_LightGrayBlue py-1 txtPrincipal m-4 focus:outline-none" placeholder='apodo'/>
+              <input type="text" name="password" className="rounded-lg text-center bg-c_LightGrayBlue py-1 txtPrincipal m-4 focus:outline-none" placeholder='contraseña'/>
+              <input type="text" name="email" className="rounded-lg text-center bg-c_LightGrayBlue py-1 txtPrincipal m-4 focus:outline-none" placeholder='correo'/>
+              <button className="bg-c_LightGrayBlue rounded-md px-4 py-2 transition-all hover:margins">Crear</button>
+            </form>
+
+          </div>
+          <div className="w-full h-2/7 flexAllCenter flex-col justify-around">
+            <button className="bg-c_LightGrayBlue w-1/6 h-1/3 rounded-lg transition-all hover:font-bold hover:bg-c_DarckBlue hover:margins">Invitado</button>
+            <div className="flexAllCenter flex-col">
+              <p className="">Ya tienes una cuenta?</p>
+              <button className="text-c_Pink hover:font-bold hover:text-white transition-all" onClick={()=>setMode(1)}>Iniciar seción</button>
+            </div>
+          </div>
+        </div>
+        <button className="bg-c_GrayBlue fixed rounded-full w-10 h-10 bottom-5 left-5 transition-all hover:bg-c_Pink"><Link href={"/support"}>?</Link></button>
+      </section>
     </Layout>
   )
 }
