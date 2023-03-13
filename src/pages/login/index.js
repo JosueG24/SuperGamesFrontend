4ec89b777583155ee792bc1d93c6a2f24c108327
JosueGuzman <107532411+JosueElutiche24@users.noreply.github.com>
2023-03-13 17:47:24 -0600

    Se implementaron modales para la zona de login

--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -6,18 +6,16 @@ import { LogInService, SingUpService, guestService } from "@/components/login/fo
 export default function Login() {
 
   const [mode, setMode] = useState(1)
+  const [modal, setModal] = useState(false)
 
   async function ImGuest(){
     const result = await guestService()
     if(result == true){
       // operar que se inicio secion
+      setModal({message:"Se ha iniciado sesión correctamente", errors:[], type:"ok"})  
     }
     //operar que hay errores con el objeto result
-    console.log(result)
-    
-    // apuntes :
-    // validaciones bien, hay que crear el mensaje de advertencia si existen errores
-    // el sevidor responde con codigo 200 a /login/guest pero no establece la cookie
+    setModal({message:"han ocurrido algunos errores", errors:[result], type:"error"})
   }
   async function handleLogIn(e){
     e.preventDefault()
@@ -26,9 +24,10 @@ export default function Login() {
     const result = await LogInService({name, password})
     if(result == true){
       // operar que se inicio secion
+      setModal({message:"Se ha iniciado sesión correctamente", errors:[], type:"ok"})  
     }
     //operar que hay errores con el objeto result
-    console.log(result)
+    setModal({message:"han ocurrido algunos errores", errors:result, type:"error"})
   }
   async function handleSingUp(e){
     e.preventDefault()
@@ -38,9 +37,10 @@ export default function Login() {
     const result = await SingUpService({name, password, email})
     if(result == true){
       // operar que se inicio secion
+      setModal({message:"Se ha iniciado sesión correctamente", errors:[], type:"ok"})  
     }
     //operar que hay errores con el objeto result
-    console.log(result)
+    setModal({message:"han ocurrido algunos errores", errors:result, type:"error"})
   }
 
   if(mode == 1)
@@ -52,14 +52,12 @@ export default function Login() {
             <h1 className="txtXl2 txtSecondary">Bienvenido a super Kimis</h1>
           </div>
           <div className="bg-c_GrayBlue w-5/6 h-5/7 rounded-md flexAllCenter flex-col">
-
-          <p className=" txtLg w-full h-1/7 flexAllCenter">Bienvenido de nuevo</p>
+            <p className=" txtLg w-full h-1/7 flexAllCenter">Bienvenido de nuevo</p>
             <form className="w-1/2 h-5/7 flexAllCenter flex-col" onSubmit={handleLogIn}>
               <input type="text" name="name" className="w-3/4 rounded-lg text-center bg-c_LightGrayBlue py-1 txtPrincipal m-4 focus:outline-none" placeholder='apodo'/>
               <input type="text" name="password" className="w-3/4 rounded-lg text-center bg-c_LightGrayBlue py-1 txtPrincipal m-4 focus:outline-none" placeholder='contraseña'/>
               <button className="bg-c_LightGrayBlue rounded-md px-4 py-2 transition-all hover:margins">Enviar</button>
             </form>
-
           </div>
           <div className="w-full h-2/7 flexAllCenter flex-col justify-around">
             <button className="bg-c_LightGrayBlue w-1/6 h-1/3  rounded-lg transition-all hover:font-bold hover:bg-c_DarckBlue hover:margins" onClick={ImGuest}>Invitado</button>
@@ -71,6 +69,17 @@ export default function Login() {
         </div>
         <Link  className="bg-c_GrayBlue fixed rounded-full w-10 h-10 bottom-5 left-5 flexAllCenter transition-all hover:bg-c_Pink" href={"/support"}>?</Link>
       </section>
+      {modal !== false &&
+      <div className="top-0 right-0 fixed flexAllCenter w-screen h-screen">
+        <div className={modal.type == "error"? "bg-c_GrayBlue margins outline-c_Pink w-2/4 h-1/2 flexAllCenter flex-col":"bg-c_GrayBlue margins w-2/4 h-1/2 flexAllCenter flex-col"}>
+          <p className="txtLg txtSecondary py-5">{modal.message}</p>
+          {modal.errors.map((item, index)=>{return(
+              <p className="txtMd pb-3" key={index}> - {item}</p>
+            )})}
+          <button className="bg-c_LightGrayBlue rounded-md px-4 py-2 transition-all hover:margins" onClick={modal.type == "error"?()=>setModal(false):()=>console.log("hola")}>Cerrar</button>
+        </div>
+      </div>
+      }
     </Layout>
   )
   if(mode == 2)
@@ -82,7 +91,6 @@ export default function Login() {
             <h1 className="txtXl2 txtSecondary">Bienvenido a super Kimis</h1>
           </div>
           <div className="bg-c_GrayBlue w-5/6 h-5/7 rounded-md flexAllCenter flex-col">
-
             <p className=" txtLg w-full h-1/7 flexAllCenter">Crear una cuenta</p>
             <form className="w-1/2 h-5/7 flexAllCenter flex-col" onSubmit={handleSingUp}>
               <input type="text" name="name" className="w-3/4 rounded-lg text-center bg-c_LightGrayBlue py-1 txtPrincipal m-4 focus:outline-none" placeholder='apodo'/>
@@ -90,7 +98,6 @@ export default function Login() {
               <input type="text" name="email" className="w-3/4 rounded-lg text-center bg-c_LightGrayBlue py-1 txtPrincipal m-4 focus:outline-none" placeholder='correo'/>
               <button className="bg-c_LightGrayBlue rounded-md px-4 py-2 transition-all hover:margins">Crear</button>
             </form>
-
           </div>
           <div className="w-full h-2/7 flexAllCenter flex-col justify-around">
             <button className="bg-c_LightGrayBlue w-1/6 h-1/3 rounded-lg transition-all hover:font-bold hover:bg-c_DarckBlue hover:margins" onClick={ImGuest}>Invitado</button>
@@ -102,6 +109,17 @@ export default function Login() {
         </div>
         <Link  className="bg-c_GrayBlue fixed rounded-full w-10 h-10 bottom-5 left-5 flexAllCenter transition-all hover:bg-c_Pink" href={"/support"}>?</Link>
       </section>
+      {modal !== false &&
+      <div className="top-0 right-0 fixed flexAllCenter w-screen h-screen">
+        <div className={modal.type == "error"? "bg-c_GrayBlue margins outline-c_Pink w-2/4 h-1/2 flexAllCenter flex-col":"bg-c_GrayBlue margins w-2/4 h-1/2 flexAllCenter flex-col"}>
+          <p className="txtLg txtSecondary py-5">{modal.message}</p>
+          {modal.errors.map((item, index)=>{return(
+              <p className="txtMd pb-3" key={index}> - {item}</p>
+            )})}
+          <button className="bg-c_LightGrayBlue rounded-md px-4 py-2 transition-all hover:margins" onClick={modal.type == "error"?()=>setModal(false):()=>console.log("hola")}>Cerrar</button>
+        </div>
+      </div>
+      }
     </Layout>
   )
 }
