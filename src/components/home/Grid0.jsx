7fb0f178ff1c841d81fb966bc93c37a99b6fd29c
JosueGuzman <107532411+JosueElutiche24@users.noreply.github.com>
2023-03-14 22:14:57 -0600

    Se hicieron grandes avances en la seccion home, en el lado de la sesión

---
+++ src/components/home/Grid0.jsx
@@ -0,0 +1,83 @@
+import axios from 'axios'
+import React from 'react'
+import { useState, useEffect } from 'react'
+
+export default function Grid0({tab}) {
+    const [data, setData] = useState([{userName:"----", puntos:0, nivel:0, fecha:0}, {userName:"----", puntos:0, nivel:0, fecha:0}, {userName:"----", puntos:0, nivel:0, fecha:0}, {userName:"---", puntos:0, nivel:0, fecha:0}, {userName:"----", puntos:0, nivel:0, fecha:0}, {userName:"----", puntos:0, nivel:0, fecha:0},{userName:"----", puntos:0, nivel:0, fecha:0}, {userName:"----", puntos:0, nivel:0, fecha:0},{userName:"----", puntos:0, nivel:0, fecha:0}, {userName:"----", puntos:0, nivel:0, fecha:0}])
+    const [myData, setMyData] = useState({userName:"----", puntos:0, nivel:0, fecha:0})
+    const [modal, setModal] = useState(false)
+
+    function convertToDate(date){
+        const myDate = new Date()
+        myDate.setTime(date);
+        const res =  myDate.toLocaleDateString();
+        if(res == "Invalid Date") return 0
+        return res
+    }
+
+    //useEfect
+    useEffect(() => {
+      async function req(){
+        try {
+            const res = await axios.post(process.env.NEXT_PUBLIC_URL_BACKEND+"/rancking/global",{mode:tab},{withCredentials:true})
+            if(res.status !== 200){
+                setModal({message:"han ocurrido algunos errores", errors:["Ha fallado la peticion de datos al servidor, revise su conecxión a internet."], type:"error"})  
+                return false
+            }
+            return res
+        } catch (error) {
+            setModal({message:"han ocurrido algunos errores", errors:["Ha fallado la peticion de datos al servidor, revise su conecxión a internet."], type:"error"})  
+            return false
+        }
+      }
+      req().then((res)=>{
+        if(res == false){
+            return
+        }
+        setData(res.data.data.top)
+        setMyData(res.data.data.myPosition)
+      })
+    }, [tab])
+    
+  return (
+    <>
+    <div className='bg-c_GrayBlue w-6/7 h-6/7'>
+        <div className='w-full h-1/12 flex font-semibold'>
+            <p className="h-full w-1/6 margins rounded-none outline-c_DarckBlue flexAllCenter">#</p>
+            <p className="h-full w-2/6 margins rounded-none outline-c_DarckBlue flexAllCenter" >Apodo</p>
+            <p className="h-full w-1/6 margins rounded-none outline-c_DarckBlue flexAllCenter">Puntos</p>
+            <p className="h-full w-2/6 margins rounded-none outline-c_DarckBlue flexAllCenter">Fecha</p>
+        </div>
+        <div className='w-full h-10/12'>
+        {data.map((item, index)=>{
+            return(
+                <div className={index%2 !== 0 ?'w-full h-1/10 flex':"w-full h-1/10 flex bg-c_LightGrayBlue"} key={index}>
+                    <p className="h-full w-1/6 margins rounded-none outline-c_DarckBlue flexAllCenter overflow-hidden">{index+1}</p>
+                    <p className="h-full w-2/6 margins rounded-none outline-c_DarckBlue flexAllCenter overflow-hidden" >{item.userName}</p>
+                    <p className="h-full w-1/6 margins rounded-none outline-c_DarckBlue flexAllCenter overflow-hidden">{item.puntos}</p>
+                    <p className="h-full w-2/6 margins rounded-none outline-c_DarckBlue flexAllCenter overflow-hidden">{convertToDate(item.date)}</p>
+                </div>
+            )
+        })}
+        </div>
+        <div className='w-full h-1/12 bg-c_Pink flex font-semibold'>
+            <p className="h-full w-1/6 margins rounded-none outline-c_DarckBlue flexAllCenter overflow-hidden">#</p>
+            <p className="h-full w-2/6 margins rounded-none outline-c_DarckBlue flexAllCenter overflow-hidden" >{myData.userName}</p>
+            <p className="h-full w-1/6 margins rounded-none outline-c_DarckBlue flexAllCenter overflow-hidden">{myData.puntos}</p>
+            <p className="h-full w-2/6 margins rounded-none outline-c_DarckBlue flexAllCenter overflow-hidden">{convertToDate(myData.date)}</p>
+        </div>
+    </div>
+    {modal !== false &&
+      <div className="top-0 right-0 fixed flexAllCenter w-screen h-screen">
+        <div className={modal.type == "error"? "bg-c_GrayBlue margins outline-c_Pink w-2/4 h-1/2 flexAllCenter flex-col":"bg-c_GrayBlue margins w-2/4 h-1/2 flexAllCenter flex-col"}>
+          <p className="txtLg txtSecondary py-5 text-center">{modal.message}</p>
+          {modal.errors.map((item, index)=>{return(
+              <p className="txtMd pb-3" key={index}> - {item}</p>
+            )})}
+          <button className="bg-c_LightGrayBlue rounded-md px-4 py-2 transition-all hover:margins" onClick={modal.type == "error"?()=>setModal(false):()=>{}}>Cerrar</button>
+        </div>
+      </div>
+      }
+    </>
+  )
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,25 +1,61 @@
 import Layout from '@/components/layout/Layout'
 import Link from 'next/link'
+import Grid0 from '@/components/home/Grid0'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 
 export default function Home() {
+  const [tab, setTab] = useState("memory")
+  const [userData, setUserData] = useState({userName:"----", profilePhoto:1})
+  useEffect(() => {
+      async function req(){
+        try {
+          const res = await axios.get(process.env.NEXT_PUBLIC_URL_BACKEND+"/sessionValidate",{withCredentials: true,}) 
+          if(res.status !== 200){
+              setModal({message:"han ocurrido algunos errores", errors:["Ha fallado la peticion de datos al servidor, revise su conecxión a internet."], type:"error"})  
+              return false
+          }
+          return res
+      } catch (error) {
+          setModal({message:"han ocurrido algunos errores", errors:["Ha fallado la peticion de datos al servidor, revise su conecxión a internet."], type:"error"})  
+          return false
+      }
+    }
+    req().then((res)=>{
+      if(res == false){
+          return
+      }
+      if(res.data.data === "guest"){
+        setUserData({userName:"Invitado", profilePhoto:1})  
+        return
+      }
+      setUserData({userName:res.data.data, profilePhoto:1})
+    })
+  }, [])
+  
   return (
     <Layout route='private'>
-      <section className='flex justify-center items-center h-screen flex-col'>  
-        <div className='bg-yellow-300 p-10 rounded-3xl'>
-          <h1 className="text-3xl font-bold underline">Home</h1>
-        </div>
-        <div className=' m-5 bg-blue-600 p-2 rounded-lg text-white'>
-          <Link href={"/login"}>go to login</Link>
-        </div>
+      <section className='w-screen h-screen flex'>
+        <div className='h-screen w-5/7'>
 
-        <div className=' m-5 bg-blue-400 p-2 rounded-lg text-white'>
-          <Link href={"/games/memory"}>go to memory</Link>
         </div>
-        <div className=' m-5 bg-blue-400 p-2 rounded-lg text-white'>
-          <Link href={"/games/snake"}>go to snake</Link>
-        </div>
-        <div className=' m-5 bg-blue-400 p-2 rounded-lg text-white'>
-          <Link href={"/games/mines"}>go to mines</Link>
+        <div className='h-screen w-2/7 border-l-c_GrayBlue border-l-4'>
+          <div className='bg-c_GrayBlue w-full h-1/7 flexAllCenter justify-around'>
+            <img className='w-1/5 rounded-full' src='./profilePhoto1.jpg'/>
+            <div className='flexAllCenter flex-col'>
+              <p>{userData.userName}</p>
+            </div>
+            <button className='text-c_Pink px-2 py-1 rounded-md transition-all hover:bg-c_DarckBlue'>Cerrar sesión</button>
+          </div>
+          <div className='w-full h-6/7 flexAllCenter flex-col justify-around'>
+            <p className='txtMd txtSecondary'>Las mejores puntuaciones</p>
+            <div className='w-full txtMd font-semibold flexAllCenter justify-around'>
+              <button className={tab=="memory"?"bg-c_Pink px-2 py-1 rounded-md":'bg-c_GrayBlue px-2 py-1 rounded-md'} onClick={()=>setTab("memory")}>Memoria</button>
+              <button className={tab=="mines"?"bg-c_Pink px-2 py-1 rounded-md":'bg-c_GrayBlue px-2 py-1 rounded-md'} onClick={()=>setTab("mines")}>BuscaMinas</button>
+              <button className={tab=="snake"?"bg-c_Pink px-2 py-1 rounded-md":'bg-c_GrayBlue px-2 py-1 rounded-md'} onClick={()=>setTab("snake")}>Snake</button>
+            </div>
+            <Grid0 tab={tab}/>
+          </div>
         </div>
       </section>
     </Layout>
